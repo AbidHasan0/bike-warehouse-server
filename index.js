219d1fad1f466667aebb6e2032752e1eee4007cf
AbidHasan0 <abidhasanrazvi@gmail.com>
2022-05-17 04:14:12 +0600

    project done

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ async function run() {
          res.send(bikes);
       });
 
-      //  Dynamic Route
+      //  Bike Details
       app.get('/bikeDetails/:id', async (req, res) => {
          const id = req.params.id;
          const query = { _id: ObjectId(id) };
